48ed1dcc447d29f0cb799812660249037b6ddc33
Nataly K <doublejanus4@gmail.com>
2022-09-19 21:31:08 +0300

    Assignment finished

---
+++ Final/Program.cs
@@ -0,0 +1,16 @@
+﻿Console.WriteLine("Enter a sentence: ");
+string[] input = Console.ReadLine().Split(' ');
+int size = input.Length;
+int count = 0;
+string[] array = new string[size];
+for(int i = 0; i < input.Length; i++)
+{
+    if(input[i].Length <= 3)
+    {
+        array[i] = input[i];
+        count++;
+        Console.WriteLine(array[i]);
+    }
+}
+
+Array.Resize(ref array, count);
